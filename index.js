a2e76eb4b90159c4df89affb7f60b87c425deed2
chanmyae-ab <chanmyae@aluto-benli.com>
2018-12-26 11:56:31 +0630

    asdsd

--- index.js
+++ index.js
@@ -1,22 +1,86 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var request = require('request');
-var app = express();
+/**
+ * Copyright 2017-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ *
+ * Starter Project for Messenger Platform Quick Start Tutorial
+ *
+ * Use this project as the starting point for following the
+ * Messenger Platform quick start tutorial.
+ *
+ * https://developers.facebook.com/docs/messenger-platform/getting-started/quick-start/
+ *
+ */
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.listen((process.env.PORT || 3000));
+'use strict';
 
-// Server frontpage
-app.get('/', function (req, res) {
-    res.send('This is TestBot Server');
+request({ "uri": "https://graph.facebook.com/v2.6/me/messages", "qs": { "access_token": EAAclxDQjQCUBANF33tJUFsjfi44jLya9jtyIBkGUtJUTTReumZC2HD6bJzZBCeE8pHKscBMlfX8IhGInkGQDAC2xjGt4NKYOc7xYZC8PiZC5yJ4CraZAmSi5ZBDUspiw9cvY6DrXUUgGzOiJeZCDyr5erUWJE6pfiTXB5NtS8fZBCAZDZD }, "method": "POST", "json": request_body }, (err, res, body) => { if (!err) { console.log('message sent!') } else { console.error("Unable to send message:" + err); } });
+
+// Imports dependencies and set up http server
+const
+  request = require('request'),
+  express = require('express'),
+  body_parser = require('body-parser'),
+  app = express().use(body_parser.json()); // creates express http server
+
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+
+// Accepts POST requests at /webhook endpoint
+app.post('/webhook', (req, res) => {
+
+  // Parse the request body from the POST
+  let body = req.body;
+
+  // Check the webhook event is from a Page subscription
+  if (body.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Get the webhook event. entry.messaging is an array, but
+      // will only ever contain one event, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+
+    });
+
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
+
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
 });
 
-// Facebook Webhook
-app.get('/webhook', function (req, res) {
-    if (req.query['hub.verify_token'] === 'testbot_verify_token') {
-        res.send(req.query['hub.challenge']);
+// Accepts GET requests at the /webhook endpoint
+app.get('/webhook', (req, res) => {
+
+  /** UPDATE YOUR VERIFY TOKEN **/
+  const VERIFY_TOKEN = "chatbot";
+
+  // Parse params from the webhook verification request
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+
+  // Check if a token and mode were sent
+  if (mode && token) {
+
+    // Check the mode and token sent are correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+      // Respond with 200 OK and challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+
     } else {
-        res.send('Invalid verify token');
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
     }
+  }
 });
